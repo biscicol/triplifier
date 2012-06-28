a89dee2dc727db452aba2af90459c42ed350a39d
jdeck88 <jdeck88@a8210fa3-d39b-f2bb-0447-f0ebcacf18d8>
2012-06-28 23:14:49 +0000

    Modified triplifier interface to specify whether is an Identifier or a Literal and adjusted triple file output accordingly

--- trunk/src/rest/Entity.java
+++ trunk/src/rest/Entity.java
@@ -10,6 +10,7 @@ import java.util.Set;
 public class Entity {
 	public String table;
 	public String idColumn;
+    public String idTypeColumn;
 	public VocabularyItem rdfClass;
 	public Set<Attribute> attributes;
 	
@@ -19,12 +20,22 @@ public class Entity {
      * @param pw PrintWriter used to write output to.
      */
 	void printD2RQ(PrintWriter pw) {
+
+        String tableColumn = table + "." + idColumn;
 		pw.println("map:" + classMap() + " a d2rq:ClassMap;");
 		pw.println("\td2rq:dataStorage " + "map:database;");
-		pw.println("\td2rq:uriColumn \"" + table + "." + idColumn + "\";");
-	//	pw.println("\td2rq:uriPattern \"@@" + table + "." + idColumn + "@@\";");
+        // This will use the exact value from the database, whether a valid URI or not
+		if (idTypeColumn.equalsIgnoreCase("URI")) {
+            pw.println("\td2rq:uriColumn \"" + tableColumn + "\";");
+        // This assigns the default urn:x-biscicol: patter before the identifier, ensuring it is a URI
+        } else {
+            pw.println("\td2rq:uriPattern \"@@" + tableColumn + "@@\";");
+        }
 	//	pw.println("\td2rq:uriPattern \"" + table + "/@@" + table + "." + idColumn + "|urlify@@\";");
 		pw.println("\td2rq:class <" + rdfClass.uri + ">;");
+        // ensures non-null values
+        pw.println("\td2rq:condition \"" + tableColumn + " <> ''\";");
+
 	//	pw.println("\td2rq:classDefinitionLabel \"" + table + "\";");
 		pw.println("\t.");
 		for (Attribute attribute : attributes)
--- trunk/src/run.java
+++ trunk/src/run.java
@@ -1,19 +1,24 @@
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.util.FileUtils;
+import de.fuberlin.wiwiss.d2rq.ModelD2RQ;
 import reader.ReaderManager;
+import reader.TabularDataConverter;
 import reader.plugins.*;
 
 
-public class run
-{
+public class run {
     private static void runReader(TabularDataReader reader) {
         String[] record;
 
         while (reader.hasNextTable()) {
             reader.moveToNextTable();
-            
+
             System.out.println("TABLE: " + reader.getCurrentTableName());
-            
+
             while (reader.tableHasNextRow()) {
                 record = reader.tableGetNextRow();
                 for (int cnt = 0; cnt < record.length; cnt++) {
@@ -22,92 +27,111 @@ public class run
 
                 System.out.println();
             }
-            
+
             //System.out.println();
         }
 
         System.out.println("file extension: " + reader.getFileExtensions()[0]);
-        
+
         reader.closeFile();
     }
-    
+
     private static void testFile(TabularDataReader reader, String filename) {
         if (reader.testFile(filename))
             System.out.println("Valid " + reader.getShortFormatDesc() + " file.");
         else
             System.out.println("Not a " + reader.getShortFormatDesc() + " file.");
     }
-    
+
     private static void runReaders() {
         TabularDataReader reader = new CSVReader();
         reader.openFile("sampledata/test.csv");
-        
+
         runReader(reader);
         testFile(reader, "test_file.csv");
         System.out.println();
-        
+
         reader = new ExcelReader();
         reader.openFile("sampledata/test.xls");
         runReader(reader);
         System.out.println();
-        
+
         reader.openFile("sampledata/test.xlsx");
         runReader(reader);
         System.out.println();
-        
+
         reader = new OpenDocReader();
         reader.openFile("sampledata/test.ods");
-        runReader(reader);        
+        runReader(reader);
     }
 
     public static void main(String[] args) throws Exception {
         //runReaders();
-        
+
         // create the ReaderManager and load the plugins
         ReaderManager rm = new ReaderManager();
         try {
             rm.LoadReaders();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
-        catch (FileNotFoundException e) { System.out.println(e); }
-        
+
         // print descriptions for all supported file formats
         for (TabularDataReader reader : rm)
             System.out.println(reader.getFormatDescription() + " ");
         System.out.println();
-        
+
         // open a file and print the data
         //runReader(rm.openFile("/Users/jdeck/bioValidatorSpreadsheets/biocode_fishes.xls"));
         //System.out.println(rm.getReader("DWCA").testFile("test.xls"));
         //System.out.println(Thread.currentThread().getContextClassLoader().getResource("sqlite").getFile());
-        runReader(rm.openFile("sampledata/test.xlsx"));
+        //runReader(rm.openFile("sampledata/test.xlsx"));
+
         //runReader(rm.openFile("test.xlsx"));
         //runReader(rm.openFile("test.csv", "CSV"));
         //runReader(rm.openFile("test-archive.zip", "DWCA"));
         //runReader(rm.openFile("dwca-hsu_wildlife_mammals.zip", "DWCA"));
         //runReader(rm.openFile("dwca-nysm_mammals.zip", "DWCA"));
         //runReader(rm.openFile("test-dwca", "DWCA"));
-        
-        /*TabularDataReader reader;
-        try {
-            reader = rm.openFile("test.xlsx");
-            //reader = rm.openFile("test.ods");
-            //reader = rm.openFile("test-archive.zip");
-            //reader = rm.openFile("dwca-nysm_mammals.zip");
-            //reader = rm.openFile("test-dwca");
-            //rm.openFile("357800_biocode.xls"), "jdbc:sqlite:tempdb.sqlite");
-            //rm.openFile("357800_biocode-tmp.xls"), "jdbc:sqlite:tempdb.sqlite");
 
-            TabularDataConverter tdc = new TabularDataConverter(
-                    reader, "jdbc:sqlite:tempdb.sqlite");
 
-            //tdc.setTableName("collecting_events");
-            tdc.convert();
+        // condensed method to test mapping file logic
+        // John testing some code here to read the biocode_template.xls file and apply some sample mapping
+        // that i'm tweeking on the fly with samplemapping.n3
+        rm.LoadReaders();
+        TabularDataReader tdr = rm.openFile("sampledata/biocode_template.xls");
+        TabularDataConverter tdc = new TabularDataConverter(tdr, "jdbc:sqlite:/tmp/triples.sqlite");
+        tdc.convert();
+        tdr.closeFile();
+        //
+        Model model = new ModelD2RQ(FileUtils.toURL("/Users/jdeck/IdeaProjects/triplifier/sampledata/samplemapping.n3")
+                );
+        FileOutputStream fos = new FileOutputStream("/tmp/triples.nt");
+        model.write(fos, FileUtils.langN3);
+        fos.close();
 
-            reader.closeFile();
+        /*
+ TabularDataReader reader;
+ try {
+     reader = rm.openFile("sampledata/test.xlsx");
+     //reader = rm.openFile("test.ods");
+     //reader = rm.openFile("test-archive.zip");
+     //reader = rm.openFile("dwca-nysm_mammals.zip");
+     //reader = rm.openFile("test-dwca");
+     //rm.openFile("357800_biocode.xls"), "jdbc:sqlite:tempdb.sqlite");
+     //rm.openFile("357800_biocode-tmp.xls"), "jdbc:sqlite:tempdb.sqlite");
 
-        } catch (Exception e) {
-            throw e;
-            //System.out.println(e);
-        }*/
+     TabularDataConverter tdc = new TabularDataConverter(
+             reader, "jdbc:sqlite:tempdb.sqlite");
+
+     //tdc.setTableName("collecting_events");
+     tdc.convert();
+
+     reader.closeFile();
+
+ } catch (Exception e) {
+     throw e;
+     //System.out.println(e);
+ }       */
     }
 }
