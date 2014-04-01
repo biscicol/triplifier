e773a09aa58fc6d44bdb083e9c4b920e807f9fee
John Deck <jdeck88@users.noreply.github.com>
2014-04-01 23:33:26 +0000

    Got the rdf2dwc class reading and printing output by class working

--- src/JenaTools/rdf2dwc.java
+++ src/JenaTools/rdf2dwc.java
@@ -1,21 +1,74 @@
 package JenaTools;
 
 import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.util.FileUtils;
 import org.apache.log4j.Level;
 
-import java.io.IOException;
-
-import static org.junit.Assert.assertTrue;
 
 public class rdf2dwc {
-    private static Model inputRDF;
-    private static String baseuri;
-    private static String classOutputFileName;
+    private String baseuri;
+    Model model;
+    QueryExecution qe;
+
+    /**
+     * Constructor reads the test File
+     */
+    public rdf2dwc(String testFile, String lang) {
+        // The base URI for all input and output for these tests
+        baseuri = "file://" + System.getProperty("user.dir") + "/testdata/";
+
+        // A model for the class tests. The MeasuringStick is the output that we expect
+        model = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM_RULES_INF);
+        model.read(baseuri + testFile, lang);
+    }
+
+    /**
+     * Get all properties of the specified DwCClass
+     *
+     * @param queryClass
+     * @return a jena ResultSet
+     * @throws Exception
+     */
+    public com.hp.hpl.jena.query.ResultSet getDwcClass(String queryClass) throws Exception {
+
+        String queryString =
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  " +
+                        "select ?s ?p ?o " +
+                        "where { " +
+                        "?s ?p ?o . " +
+                        "?s a <" + queryClass + "> . " +
+                        "FILTER(isLiteral(?o)) . " +
+                        "} \n ";
+        // Create a new query
+        Query query = QueryFactory.create(queryString);
+
+        // Execute the query and obtain results
+        qe = QueryExecutionFactory.create(query, model);
+        com.hp.hpl.jena.query.ResultSet results = qe.execSelect();
+        return results;
+    }
+
+    /**
+     * Print the output of a resultSet... This is just an example of how to work with resultSet
+     *
+     * @param resultSet
+     */
+    public void printer(ResultSet resultSet) {
+        for (; resultSet.hasNext(); ) {
+            QuerySolution soln = resultSet.nextSolution();
+            RDFNode x = soln.get("s");
+            Resource r = soln.getResource("p");
+            Literal l = soln.getLiteral("o");
+
+            // Print output as an example here
+            System.out.println(x.toString() + " " + r.toString() + " " + l.toString());
+        }
+        // close the Query after printing.
+        qe.close();
+    }
 
     /**
      * for testing, non-static methods should typically control class
@@ -24,41 +77,12 @@ public class rdf2dwc {
         // Set logging output level
         org.apache.log4j.Logger.getRootLogger().setLevel(Level.ERROR);
 
-        // DO THIS BY HAND! Set the output file for the results of triplifying classInput.txt
-        // User-interface output
-        //classOutputFileName = "classOutputUserInterface.ttl";
+        rdf2dwc r = new rdf2dwc("rdf2dwcTest.ttl", FileUtils.langTurtle);
 
-
-        // The base URI for all input and output for these tests
-        baseuri = System.getProperty("user.dir");
-        baseuri = "file://" + baseuri + "/testdata/";
-
-        // A model for the class tests. The MeasuringStick is the output that we expect
-        inputRDF = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM_RULES_INF);
-        inputRDF.read(baseuri + "rdf2dwcTest.ttl", null, FileUtils.langNTriple);
-
-        rdf2dwc r = new rdf2dwc();
-        r.loopStatements();
+        // Output query results
+        r.printer(r.getDwcClass("dwc:Occurrence"));
+        r.printer(r.getDwcClass("dwc:Taxon"));
     }
-
-    /**
-    * Loop statements
-     * @throws Exception
-     */
-    public void loopStatements() throws Exception {
-        StmtIterator msIter = inputRDF.listStatements();
-
-        while (msIter.hasNext()) {
-            Statement msStmt = msIter.nextStatement();
-            String subject = msStmt.getSubject().toString();
-            String predicate = msStmt.getPredicate().toString();
-            String object = msStmt.getObject().toString();
-            System.out.println(subject + " " + predicate + " " + object);
-        }
-
-    }
-
-
 
 }
 
