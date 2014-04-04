74f99dd3ab2f8a5daef0be098a9616ee1cc4153e
jdeck88 <jdeck88@a8210fa3-d39b-f2bb-0447-f0ebcacf18d8>
2014-04-04 08:52:05 +0000

    Created more generalized class structure (rdf2dwc and rdf2isa both extend rdfConversionTools) Enabled the ability to specify which DwC to write to, for instance can write data to MaterialSample Core.

---
+++ trunk/src/JenaTools/rdf2isa.java
@@ -0,0 +1,80 @@
+package JenaTools;
+
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.util.FileUtils;
+import org.apache.log4j.Level;
+import java.io.File;
+
+/**
+ * Conversion of input RDF data to ISATab Specification
+ */
+public class rdf2isa extends rdfConversionTools {
+
+    /**
+     * Constructor reads the test File
+     */
+    public rdf2isa(String inputFile, String lang, Integer maxDepth,Resource rootResource, File outputDir) {
+        super(inputFile, lang, maxDepth,  rootResource, outputDir);
+    }
+
+
+
+    /**
+     * Print the output of a resultSet... This is just an example of how to work with resultSet
+     *
+     * @param resultSet
+     */
+    public void printer(ResultSet resultSet) {
+        for (; resultSet.hasNext(); ) {
+            QuerySolution soln = resultSet.nextSolution();
+            rootResource = soln.getResource("s");
+             printMyLiterals(rootResource);
+
+        }
+        // close the Query after printing.
+        qe.close();
+    }
+    public void printMyLiterals(Resource resource) {
+        currentDepth++;
+         StmtIterator s = resource.listProperties();
+            while (s.hasNext()) {
+                Statement s2 = s.nextStatement();
+
+                Resource x2 = s2.getSubject();
+                Resource r2 = s2.getPredicate();
+                RDFNode l2 = s2.getObject();
+                if (l2.isLiteral())
+                    System.out.println(rootResource + " " + r2.toString() + " " + l2);
+
+                if (!l2.isLiteral() && currentDepth < maxDepth)
+                    printMyLiterals(l2.asResource());
+            }
+
+         currentDepth--;
+    }
+
+    /**
+     * for testing, non-static methods should typically control class
+     */
+    public static void main(String[] args) throws Exception {
+        // Set logging output level
+        org.apache.log4j.Logger.getRootLogger().setLevel(Level.ERROR);
+
+
+        String inputFile = "file://" + System.getProperty("user.dir") + "/testdata/rdf2dwcTest.ttl";
+
+        rdf2isa r = new rdf2isa(
+                inputFile,
+                FileUtils.langTurtle,
+                3,
+                ResourceFactory.createResource("dwc:Occurrence"),
+                org.gbif.utils.file.FileUtils.createTempDir());
+
+        // Output query results
+        r.printer(r.getRootResources());
+        //r.printer(r.getDwcClass("dwc:Taxon"));
+    }
+
+}
+
---
+++ trunk/src/JenaTools/rdfConversionTools.java
@@ -0,0 +1,73 @@
+package JenaTools;
+
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Reads RDF files and provides methods for tools meant to convert RDF data into some other format.
+ */
+public class rdfConversionTools {
+    protected String inputFile;
+    protected Model model;
+    protected QueryExecution qe;
+    protected Integer currentDepth = 0;
+    protected Integer maxDepth = 99;
+    protected Resource rootResource;
+    protected File outputDir;
+
+    /**
+     * Constructor does the actual reading
+     */
+    public rdfConversionTools(String inputFile, String lang, Integer maxDepth,Resource rootResource, File outputDir) {
+        this.inputFile = inputFile;
+        this.maxDepth = maxDepth;
+        this.outputDir = outputDir;
+        this.rootResource = rootResource;
+        // A model for the class tests. The MeasuringStick is the output that we expect
+        model = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM_RULES_INF);
+        model.read(inputFile, lang);
+    }
+
+    /**
+     * Get all properties of the specified DwCClass
+     *
+     * @return a jena ResultSet
+     * @throws Exception
+     */
+    public ResultSet getRootResources() throws Exception {
+
+        String queryString =
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  " +
+                        "select ?s ?p ?o " +
+                        "where { " +
+                        "?s ?p ?o . " +
+                        "?s a <" + rootResource.toString() + "> . " +
+                        "FILTER(isLiteral(?o)) . " +
+                        "} \n ";
+        // Create a new query
+        System.out.println(queryString);
+        Query query = QueryFactory.create(queryString);
+
+        // Execute the query and obtain results
+        qe = QueryExecutionFactory.create(query, model);
+        com.hp.hpl.jena.query.ResultSet results = qe.execSelect();
+        return results;
+    }
+
+    /**
+     * Meant to be over-written
+     * @param resultSet
+     * @throws IOException
+     */
+    public void printer(ResultSet resultSet) throws IOException {
+
+    }
+
+
+
+}
